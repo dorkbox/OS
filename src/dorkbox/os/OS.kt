244ac97eaba15a19ffe58d858f8bc07a6e6f84ab
Robinson <robinson@dorkbox.com>
2023-08-19 12:55:07 +0200

    version 1.8

--- src/dorkbox/os/OS.kt
+++ src/dorkbox/os/OS.kt
@@ -27,7 +27,7 @@ object OS {
     /**
      * Gets the version number.
      */
-    const val version = "1.7"
+    const val version = "1.8"
 
     init {
         // Add this project to the updates system, which verifies this class + UUID + version information
