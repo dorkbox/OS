d84d4380c7073f2228a732d7d311f1102696ad9d
Robinson <robinson@dorkbox.com>
2026-01-29 15:11:04 +0100

    removed JPMS/multi-jar

--- src/dorkbox/os/JVM.kt
+++ src/dorkbox/os/JVM.kt
@@ -1,10 +1,24 @@
+/*
+ * Copyright 2026 dorkbox, llc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dorkbox.os
 
 internal object JVM {
-    // java 8 cannot use JPMS. We use a multi-release jar to detect the state for java9+ runtimes
-
     /**
      * Returns true if the currently running JVM is using the classpath or modules (JPMS)
      */
-    var usesJpms = false
+    var usesJpms = java.lang.ModuleLayer.boot().findModule("java.desktop").isPresent
 }
--- src9/dorkbox/os/JVM.kt
+++
@@ -1,8 +0,0 @@
-package dorkbox.os
-
-internal object JVM {
-    /**
-     * Returns true if the currently running JVM is using the classpath or modules (JPMS)
-     */
-    var usesJpms = true == java.lang.ModuleLayer.boot().findModule("java.desktop").isPresent
-}
